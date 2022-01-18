da234500f4e4d129b96feee09a898b71fcf67c23
Meron Ambelu <48333207+mambelu@users.noreply.github.com>
2022-01-18 12:43:25 -0500

    Add files via upload

---
+++ Organic-Market-2/src/main/java/com/freshCoders/OrganicMarket2/OrganicMarket2Application.java
@@ -0,0 +1,13 @@
+package com.freshCoders.OrganicMarket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicMarket2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicMarket2Application.class, args);
+	}
+
+}
---
+++ Organic-Market-2/src/test/java/com/freshCoders/OrganicMarket2/OrganicMarket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshCoders.OrganicMarket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicMarket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
