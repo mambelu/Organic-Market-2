9c3025a8e76b271e90d53c264b62de1b8108d8f6
NYS <turbo@NYSs-MacBook-Pro.local>
2022-01-26 12:23:47 -0600

    created service interface and class

---
+++ Organic-Market-2/src/main/java/com/freshCoders/OrganicMarket2/Service/ProductService.java
@@ -0,0 +1,11 @@
+package com.freshCoders.OrganicMarket2.Service;
+
+import com.freshCoders.OrganicMarket2.Entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+
+   List<Product> all();
+}
